e65d4f6f8d8c4572c186598559d6f0d8c9ad789c
HuwaCoffee <116842137+HuwaCoffee@users.noreply.github.com>
2022-10-28 03:56:31 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+typedef char ElemType;
+
+//栈的定义
+typedef struct
+{
+	ElemType* base;
+	ElemType* top;
+	//int stackSize;
+}sqStack;
+
+//栈的初始化
+void InitStack(sqStack* s)
+{
+	s->base = new ElemType();
+	if (s->base == NULL) {
+		exit(0);
+	}
+	s->top = s->base;
+	//s->stackSize = 0;
+}
+
+//压栈操作
+void push(sqStack* s, ElemType r)
+{
+	*s->top = r;
+	s->top++;
+}
+
+//弹栈操作
+void pop(sqStack* s,ElemType*r) {
+	if (s->base == s->top) {
+		return;
+	}
+	*r = *--(s->top);
+}
+
+//求栈当前容量
+int StackLen(sqStack s)
+{
+	return (s.top - s.base);
+}
+
+int main()
+{
+	ElemType c;
+	sqStack s;
+	InitStack(&s);
+	int len, i, sum = 0;
+	cout << "请输入一个二进制数，以#结尾代表结束：";
+	scanf_s("%c", &c);
+	while (c != '#') {
+		push(&s, c);
+		scanf_s("%c", &c);
+	}
+	getchar();
+	len = StackLen(s);
+	for (i = 0; i < len; i++) {
+		pop(&s, &c);
+		sum += (c-48) * pow(2, i);
+	}
+	cout << "转化为十进制为：" << sum << endl;
+}
